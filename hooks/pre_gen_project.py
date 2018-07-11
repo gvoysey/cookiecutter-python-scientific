e17bcc491954c7097cdb47a15121b52d06dedb6a
Graham Voysey <graham@neurala.com>
2018-07-10 20:11:57 -0400

    testing precommits

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -9,11 +9,19 @@ def check_prerequisites():
         run('git --version'.split(), check=True)
     except CalledProcessError as e:
         print('git is required!')
-
+        raise FileNotFoundError
     try:
         run('pipenv --version'.split(), check=True)
     except CalledProcessError as e:
         print('pipenv is required!')
+        res = input('Attempt to install pipenv? [Y/n]: ')
+        if not res or res.casefold() == 'y':
+            try:
+                pipenv_install = run('pip install --user pipenv'.split(), check=True)
+            except CalledProcessError:
+                raise FileNotFoundError
+        else:
+            raise FileNotFoundError
 
 def main():
     check_prerequisites()
